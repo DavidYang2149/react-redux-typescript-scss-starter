ed770dc5107f8003ad26178c173d5edc04b3484c
davidyang2149 <davidyang2149@gmail.com>
2021-04-25 23:07:50 +0900

    Add Initialize Files & Folders 🏠
    RRTS Starter
    - Initialize Files & Folders.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="sample">Hello World!</h1>
+      <p>안녕하세요</p>
+    </div>
+  );
+}
