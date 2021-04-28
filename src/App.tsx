7357597a8e8b6b2d38468af496aa5aedc2e461b4
davidyang2149 <davidyang2149@gmail.com>
2021-04-28 22:15:59 +0900

    Update App (Add Router)
    RRTS Starter
    - Update App (Add Router).

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.scss';
+
+import NotFoundPage from './pages/common/NotFoundPage';
+import HelloPage from './pages/common/HelloPage';
+import TodosPage from './pages/todo/TodosPage';
 
 export default function App() {
   return (
-    <div>
-      <h1 className="sample">Hello World!</h1>
-      <p>안녕하세요</p>
-    </div>
+    <>
+      <h1 className="sample">Hello Sample Todo</h1>
+      <Switch>
+        <Route exact path="/" component={TodosPage} />
+        <Route exact path="/Hello" component={HelloPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </>
   );
 }
