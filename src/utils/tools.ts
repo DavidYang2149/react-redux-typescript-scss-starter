7742cf88da78110659c435a0c1a7d757ff4f7d75
davidyang2149 <davidyang2149@gmail.com>
2021-04-28 22:38:36 +0900

    Add tools utils 📂
    RRTS Starter
    - Add tools utils.
      - Add function isEmpty.

---
+++ src/utils/tools.ts
@@ -0,0 +1,10 @@
+export const isEmpty = (value: unknown) => {
+  if (!value) {
+    return true;
+  }
+  return false;
+};
+
+export const XXX = () => {
+  // TODOS: Add function & Remove This funciton
+};
