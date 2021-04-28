92901bd11731b33df38ca5ecf8349fbac22e28e8
davidyang2149 <davidyang2149@gmail.com>
2021-04-28 22:48:54 +0900

    Add todos fixtures 📂 for TestCode 🛠
    RRTS Starter
    - Add todos fixtures.
      - for TestCode.
      - todos sample.

---
+++ fixtures/todos.ts
@@ -0,0 +1,16 @@
+const todos = [
+  {
+    id: 1,
+    content: 'sample1',
+  },
+  {
+    id: 2,
+    content: 'sample2',
+  },
+  {
+    id: 3,
+    content: 'sample3',
+  },
+];
+
+export default todos;
