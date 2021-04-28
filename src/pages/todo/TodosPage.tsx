efc595267831de62fe95918c831cabf263eea46d
davidyang2149 <davidyang2149@gmail.com>
2021-04-28 22:41:39 +0900

    Add TodosPage, HelloPage, NotFoundPage Page 📂
    RRTS Starter
    - Add Pages.
      - TodoPage.
      - HelloPage.
      - NotFoundPage.

---
+++ src/pages/todo/TodosPage.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import TodosContainer from 'src/containers/todo/TodosContainer';
+import { loadTodos } from 'src/store/todo/todos';
+
+const TodosPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTodos());
+  });
+
+  return (
+    <>
+      <h2>TodosPage</h2>
+      <TodosContainer />
+    </>
+  );
+};
+
+export default TodosPage;
