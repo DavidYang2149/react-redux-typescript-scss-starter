1df67902208b7a480d639e5a5a27af056e79b573
davidyang2149 <davidyang2149@gmail.com>
2021-04-29 17:39:22 +0900

    Update TodosContainer containers (Add TodoAdd)
    RRTS Starter
    - Update TodosContainer containers.
      - Add TodoAdd with Logic.

--- src/containers/todo/TodosContainer.tsx
+++ src/containers/todo/TodosContainer.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { RootState } from 'src/store/rootReducer';
+import TodoAdd from 'src/components/todo/TodoAdd';
 import TodoList from 'src/components/todo/TodoList';
+import { RootState } from 'src/store/rootReducer';
+import { changeTodo } from 'src/store/todo/todo';
 
 const TodosContainer = () => {
-  const { todos: { todos } } = useSelector((state: RootState) => ({
+  const dispatch = useDispatch();
+
+  const { todo, todos: { todos } } = useSelector((state: RootState) => ({
+    todo: state.todo,
     todos: state.todos,
   }));
+
+  const onChangeTodoAdd = (event: { target: HTMLInputElement }) => {
+    const { name, value } = event.target;
+    dispatch(changeTodo({ name, value }));
+  };
 
   return (
     <>
       <h3>TodosContainer</h3>
+      <TodoAdd
+        todo={todo}
+        onChange={onChangeTodoAdd}
+      />
       <TodoList
         todos={todos}
       />
