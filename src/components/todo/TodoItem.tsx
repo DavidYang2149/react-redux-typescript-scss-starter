8222cf3a8ef885f6ebe2c6874a727b8d8f9ed8ef
davidyang2149 <davidyang2149@gmail.com>
2021-04-29 11:13:02 +0900

    Update TodoItem components (Add id empty case)
    RRTS Starter
    - Update TodoItem components
      - Add logic for id empty case.

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 import { Todo } from 'src/types/todo';
+import { isEmpty } from 'src/utils/tools';
 
-const TodoItem = ({ content }: Todo) => {
+const TodoItem = ({ id, content }: Todo) => {
+  if (isEmpty(id)) {
+    return (
+      <></>
+    );
+  }
+
   return (
     <p>{content}</p>
   );
