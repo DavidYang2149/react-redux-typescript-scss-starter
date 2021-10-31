cd337aec182a336a54a3c60cf31595e6454d6cc0
davidyang2149 <davidyang2149@gmail.com>
2021-10-31 21:48:48 +0900

    :white_check_mark: E2E Todo 추가하기를 테스트합니다.
    RRTS Starter

--- cypress/integration/001-main/001-main.spec.js
+++ cypress/integration/001-main/001-main.spec.js
@@ -6,4 +6,11 @@ describe('메인 페이지 접속하기', () => {
   it('메인 페이지를 확인합니다.', () => {
     cy.get('.sample').should('have.text', 'Hello Sample TodoThis is Custom Font');
   });
+
+  it('새로운 Todo를 추가합니다.', () => {
+    cy.get('#newTodo').type('test todo를 추가합니다.');
+    cy.contains('Add Button').click();
+
+    cy.contains('test todo를 추가합니다.').should('exist');
+  });
 });
